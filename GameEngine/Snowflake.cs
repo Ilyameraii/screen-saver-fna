744eb9f8d17db46578fec7c96274a20583712b4d
Ilya Bobrov <ilyaraibusiness@gmail.com>
2025-11-07 17:38:15 +0300

    убрал лишний using в менеджере

--- GameEngine/Snowflake.cs
+++ GameEngine/Snowflake.cs
@@ -1,20 +1,18 @@
-﻿namespace GameEngine
+﻿namespace GameEngine;
+internal class Snowflake
 {
-    internal class Snowflake
-    {
-        /// <summary>
-        /// Свойство положения снежинки по x
-        /// </summary>
-        public  int X { get; set; }
+    /// <summary>
+    /// Свойство положения снежинки по x
+    /// </summary>
+    public  int X { get; set; }
 
-        /// <summary>
-        /// Свойство положения снежинки по y
-        /// </summary>
-        public int Y { get; set; }
+    /// <summary>
+    /// Свойство положения снежинки по y
+    /// </summary>
+    public int Y { get; set; }
 
-        /// <summary>
-        /// Свойство длины стороны снежинки
-        /// </summary> 
-        public int Size { get; set; }
-    }
+    /// <summary>
+    /// Свойство длины стороны снежинки
+    /// </summary> 
+    public int Size { get; set; }
 }
--- GameEngine/SnowflakeManager.cs
+++ GameEngine/SnowflakeManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Drawing;
-
-namespace GameEngine
+﻿namespace GameEngine
 {
     internal class SnowflakeManager
     {
