d5d29d8f0111feaff313f07354355d98683ffbe0
Ilya Bobrov <ilyaraibusiness@gmail.com>
2025-11-07 16:47:12 +0300

    Создал класс снежинок, и класс менеджер снежинок

---
+++ GameEngine/SnowfallManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+
+namespace GameEngine
+{
+    internal class SnowfallManager
+    {
+        /// <summary>
+        /// Количество снежинок
+        /// </summary>
+        private const int AmountOfSnowflakes = 1500;
+
+        /// <summary>
+        /// Коэффициент скорости движения снежинок
+        /// </summary>
+        private const float SpeedCoefficient = 0.1f;
+
+        /// <summary>
+        /// Минимальный размер снежинки
+        /// </summary>
+        private const int MinSnowflakeSize = 10;
+
+        /// <summary>
+        /// Максимальный размер снежинки
+        /// </summary>
+        private const int MaxSnowflakeSize = 30;
+
+        private readonly int screenWidth;
+        private readonly int screenHeight;
+
+        private readonly Random random = new Random();
+
+        public SnowfallManager(int screenWidth, int screenHeight)
+        {
+            this.screenWidth = screenWidth;
+            this.screenHeight = screenHeight;
+            CreateSnowFlakes();
+        }
+        public List<Snowflake> Snowflakes { get; private set; } = new();
+
+        private void CreateSnowFlakes()
+        {
+            for (var i = 0; i < AmountOfSnowflakes; i++)
+            {
+                var snowflake = new Snowflake
+                {
+                    Size = random.Next(MinSnowflakeSize, MaxSnowflakeSize),
+                    
+                };
+                snowflake.X = random.Next(-snowflake.Size, screenWidth + snowflake.Size);
+                snowflake.Y = random.Next(-screenHeight - snowflake.Size, -snowflake.Size);
+                Snowflakes.Add(snowflake);
+            }
+        }
+        public void Update()
+        {
+            foreach (var snowflake in Snowflakes)
+            {
+                snowflake.Y += snowflake.Size * SpeedCoefficient;
+                if (snowflake.Y > screenHeight + snowflake.Size)
+                {
+                    snowflake.Y = -snowflake.Size;
+                    snowflake.X = random.Next(-snowflake.Size, screenWidth + snowflake.Size);
+                }
+            }
+        }
+    }
+}
---
+++ GameEngine/Snowflake.cs
@@ -0,0 +1,20 @@
+﻿namespace GameEngine
+{
+    internal class Snowflake
+    {
+        /// <summary>
+        /// Свойство положения снежинки по x
+        /// </summary>
+        public float X { get; set; }
+
+        /// <summary>
+        /// Свойство положения снежинки по y
+        /// </summary>
+        public float Y { get; set; }
+
+        /// <summary>
+        /// Свойство длины стороны снежинки
+        /// </summary> 
+        public int Size { get; set; }
+    }
+}
